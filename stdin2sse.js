6c4ea3446fc97099fb91726a356301a7fbe571d8
Joe Gaffey <joe@joegaffey.com>
2014-10-25 17:18:20 +0100

    Moved imports to top of file

--- stdin2sse.js
+++ stdin2sse.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var SSE = require('express-sse');
+var readline = require('readline');
 
 var app = express();
 app.listen(8001);
 
-var SSE = require('express-sse');
 var sse = new SSE();
 app.get('/events', sse.init);
 
 app.use(express.static(__dirname + '/'));
-
-var readline = require('readline');
 
 var rl = readline.createInterface({
   input: process.stdin,
